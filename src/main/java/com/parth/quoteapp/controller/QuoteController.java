6ff2c78534c52f9afa1ee22271a534948423cc52
parthsoni741 <parthsoni742001@gmail.com>
2026-01-10 19:05:04 -0500

    Return quote as JSON using model

--- src/main/java/com/parth/quoteapp/controller/QuoteController.java
+++ src/main/java/com/parth/quoteapp/controller/QuoteController.java
@@ -1,4 +1,16 @@
 package com.parth.quoteapp.controller;
 
+import com.parth.quoteapp.model.Quote;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class QuoteController {
+
+    @GetMapping("/api/quotes")
+    public Quote getQuote(){
+        return new Quote(
+                "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+                "Winston Churchill");
+    }
 }
---
+++ src/main/java/com/parth/quoteapp/model/Quote.java
@@ -0,0 +1,18 @@
+package com.parth.quoteapp.model;
+
+public class Quote {
+    private String text;
+    private String author;
+
+    public Quote(String text, String author){
+        this.text=text;
+        this.author=author;
+    }
+
+    public String getText(){
+        return text;
+    }
+    public String getAuthor(){
+        return author;
+    }
+}
